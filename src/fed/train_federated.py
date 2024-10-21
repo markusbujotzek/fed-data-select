b8be8392191ec208019c80ff2a38a6f058bb46ca
Bujotzek, Markus <markus.bujotzek@dkfz-heidelberg.de>
2024-10-21 16:59:36 +0200

    adapted dataloaders for uneven fed_camelyon16 images

--- src/fed/train_federated.py
+++ src/fed/train_federated.py
@@ -23,18 +23,34 @@ def import_dataset(ds_name):
             BaselineLoss,
             metric,
             NUM_CLIENTS,
-            get_nb_max_rounds
+            get_nb_max_rounds,
         )
         from flamby.datasets.fed_heart_disease import FedHeartDisease as FedDataset
 
     elif ds_name == "FedCamelyon16":
-        print(0)
-        #TODO get fedcamelyon16 dataset
+        global collate_fn
+        from flamby.datasets.fed_camelyon16 import (
+            BATCH_SIZE,
+            LR,
+            NUM_EPOCHS_POOLED,
+            Baseline,
+            BaselineLoss,
+            metric,
+            NUM_CLIENTS,
+            get_nb_max_rounds,
+        )
+        from flamby.datasets.fed_camelyon16 import FedCamelyon16 as FedDataset
+
+        # dataset requires padding implemented in collate_fn
+        from flamby.datasets.fed_camelyon16 import collate_fn
+
     # TODO implement more datasets
 
     else:
         # Raise an exception if an unsupported dataset name is provided
-        raise ValueError(f"Dataset '{ds_name}' is not supported. Please provide a valid dataset name.")
+        raise ValueError(
+            f"Dataset '{ds_name}' is not supported. Please provide a valid dataset name."
+        )
 
 
 def import_n_set_agg_strategy(agg_strategy_name):
@@ -49,14 +65,16 @@ def import_n_set_agg_strategy(agg_strategy_name):
     if agg_strategy_name == "FedAvg":
         # 1st line of code to change to switch to another strategy
         from flamby.strategies.fed_avg import FedAvg as strat
+
         # TODO import aggregation strategy related hparams
 
-    #TODO: implement more strategies
+    # TODO: implement more strategies
 
     else:
         # Raise an exception if an unsupported strategy name is provided
-        raise ValueError(f"Federated aggregation strategy '{agg_strategy_name}' is not supported. Please provide a valid strategy name.")
-
+        raise ValueError(
+            f"Federated aggregation strategy '{agg_strategy_name}' is not supported. Please provide a valid strategy name."
+        )
 
 
 def train_federated(args):
@@ -68,62 +86,98 @@ def train_federated(args):
     print(f"Federated aggregation strategy set: {args.agg_strategy}")
 
     # We loop on all the clients of the distributed dataset and instantiate associated data loaders
-    train_dataloaders = [
-                torch.utils.data.DataLoader(
-                    FedDataset(center = i, train = True, pooled = False),
-                    batch_size = BATCH_SIZE,
-                    shuffle = True,
-                    num_workers = 0
-                )
-                for i in range(NUM_CLIENTS)
-            ]
+    if args.dataset == "FedCamelyon16":
+        train_dataloaders = [
+            torch.utils.data.DataLoader(
+                FedDataset(center=i, train=True, pooled=False),
+                batch_size=BATCH_SIZE,
+                shuffle=True,
+                num_workers=0,
+                collate_fn=collate_fn,  # Use collate_fn only for FedCamelyon16
+            )
+            for i in range(NUM_CLIENTS)
+        ]
+    else:
+        train_dataloaders = [
+            torch.utils.data.DataLoader(
+                FedDataset(center=i, train=True, pooled=False),
+                batch_size=BATCH_SIZE,
+                shuffle=True,
+                num_workers=0,
+            )
+            for i in range(NUM_CLIENTS)
+        ]
 
+    # set loss and model
     lossfunc = BaselineLoss()
     m = Baseline()
 
     # Federated Learning loop
     # 2nd line of code to change to switch to another strategy (feed the FL strategy the right HPs)
     args = {
-                "training_dataloaders": train_dataloaders,
-                "model": m,
-                "loss": lossfunc,
-                "optimizer_class": torch.optim.SGD,
-                "learning_rate": LR / 10.0,
-                "num_updates": 100,
-    # This helper function returns the number of rounds necessary to perform approximately as many
-    # epochs on each local dataset as with the pooled training
-                "nrounds": get_nb_max_rounds(100),
-            }
+        "training_dataloaders": train_dataloaders,
+        "model": m,
+        "loss": lossfunc,
+        "optimizer_class": torch.optim.SGD,
+        "learning_rate": LR / 10.0,
+        "num_updates": 100,
+        # This helper function returns the number of rounds necessary to perform approximately as many
+        # epochs on each local dataset as with the pooled training
+        "nrounds": get_nb_max_rounds(100),
+    }
     s = strat(**args)
 
-    # full training is performed according to and in defined strategy
+    # full federated training is performed according to and in defined strategy
     m = s.run()[0]
 
     # Evaluation
     # We only instantiate one test set in this particular case: the pooled one
-    test_dataloaders = [
-                torch.utils.data.DataLoader(
-                    FedDataset(train = False, pooled = True),
-                    batch_size = BATCH_SIZE,
-                    shuffle = False,
-                    num_workers = 0,
-                )
-            ]
+    if args.dataset == "FedCamelyon16":
+        test_dataloaders = [
+            torch.utils.data.DataLoader(
+                FedDataset(train=False, pooled=True),
+                batch_size=BATCH_SIZE,
+                shuffle=False,
+                num_workers=0,
+                collate_fn=collate_fn,  # Use collate_fn only for FedCamelyon16
+            )
+        ]
+    else:
+        test_dataloaders = [
+            torch.utils.data.DataLoader(
+                FedDataset(train=False, pooled=True),
+                batch_size=BATCH_SIZE,
+                shuffle=False,
+                num_workers=0,
+            )
+        ]
     dict_cindex = evaluate_model_on_tests(m, test_dataloaders, metric)
     print(dict_cindex)
+
 
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--dataset', type=str, required=True, default='FedHeartDisease',
-        help='[REQUIRED] Name of the Dataset that should be used. Default is FedHeartDisease.')
-    parser.add_argument('--agg_strategy', type=str, required=True, default='FedAvg',
-        help='[REQUIRED] Name of the federated aggregation strategy that should be used. Default is FedAvg.')
-    
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        required=True,
+        default="FedHeartDisease",
+        help="[REQUIRED] Name of the Dataset that should be used. Default is FedHeartDisease.",
+    )
+    parser.add_argument(
+        "--agg_strategy",
+        type=str,
+        required=True,
+        default="FedAvg",
+        help="[REQUIRED] Name of the federated aggregation strategy that should be used. Default is FedAvg.",
+    )
+
     args = parser.parse_args()
 
     return args
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = get_args()
     train_federated(args)
